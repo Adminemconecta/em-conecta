ecf3fcaaf84006cfaa8234129006c70b27353359
Adminemconecta <emconnecta@gmail.com>
2016-04-14 08:27:00 -0500

    madrugada

--- pulpasmadretierra/views/dashboard/js/index.js
+++ pulpasmadretierra/views/dashboard/js/index.js
@@ -1,4 +1,4 @@
 //backstretch
 $(document).ready(function() {
-	jQuery.backstretch(_BASE_URL+"public/img/food-healthy-meal-grain-compressor.jpg");
+	jQuery.backstretch(_BASE_URL+"public/img/fruits-market-colors-compressor.jpg");
 });
