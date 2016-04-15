0273b06a9ecbec889254c1888e75d23fd6209f37
Adminemconecta <emconnecta@gmail.com>
2016-04-15 03:29:48 -0500

    viernes madrugada

--- pulpasmadretierra/views/dashboard/js/index.js
+++ pulpasmadretierra/views/dashboard/js/index.js
@@ -1,4 +1,21 @@
 //backstretch
 $(document).ready(function() {
 	jQuery.backstretch(_BASE_URL+"public/img/fruits-market-colors-compressor.jpg");
+	$("#conf_inicial").click(function(){
+	    $(".social_user_mail").toggle("fade");
+	    $(".portafolio_card").hide("fade");
+	    $(".productos_cad").hide("fade");
+	});
+	$("#portafolio").click(function(){
+	    $(".portafolio_card").toggle("fade");
+	    $(".social_user_mail").hide("fade");
+	    $(".productos_cad").hide("fade");
+	});
+
+	$("#tienda").click(function(){
+	    $(".productos_cad").toggle("fade");
+	    $(".social_user_mail").hide("fade");
+	    $(".social_user_mail").hide("fade");
+	    $(".portafolio_card").hide("fade");
+	});
 });
